61bdd22137b66b88453ab9e84f9ae37ac0c3998d
giacomo <giacomo.sciolti@gmail.com>
2023-12-13 22:24:27 +0100

    Step 1 - Basic movements

--- src/main/kotlin/com/gsciolti/kata/marsrover/Main.kt
+++ src/main/kotlin/com/gsciolti/kata/marsrover/Main.kt
@@ -1,5 +1,40 @@
 package com.gsciolti.kata.marsrover
 
 fun main(vararg args: String) {
-    TODO("Not implemented")
+
+    val start = args.filter { it.startsWith("-start") }.first()
+    val command = args.filter { it.startsWith("-command") }.first().split("=")[1]
+
+    var x = start.split("=")[1].split(",")[0].toInt()
+    var y = start.split("=")[1].split(",")[1].toInt()
+    var d = start.split("=")[1].split(",")[2]
+
+    var msg = when (command) {
+        "f" -> "moved forward"
+        "b" -> "moved backward"
+        "l" -> "turned left"
+        "r" -> "turned right"
+        else -> TODO("Unrecognized command")
+    }
+
+    when (Pair(command, d)) {
+        "f" to "n" -> y++
+        "f" to "e" -> x++
+        "f" to "s" -> y--
+        "f" to "w" -> x--
+        "b" to "n" -> y--
+        "b" to "e" -> x--
+        "b" to "s" -> y++
+        "b" to "w" -> x++
+        "l" to "n" -> d = "w"
+        "l" to "e" -> d = "n"
+        "l" to "s" -> d = "e"
+        "l" to "w" -> d = "s"
+        "r" to "n" -> d = "e"
+        "r" to "e" -> d = "s"
+        "r" to "s" -> d = "w"
+        "r" to "w" -> d = "n"
+    }
+
+    println("Rover $msg. Current [$x,$y:$d]")
 }

2563e30ea02f22ad9290438b161906ff1ad9bd1a
giacomo <giacomo.sciolti@gmail.com>
2023-12-14 13:44:57 +0100

    Step 6 - Multiple commands

--- src/main/kotlin/com/gsciolti/kata/marsrover/Main.kt
+++ src/main/kotlin/com/gsciolti/kata/marsrover/Main.kt
@@ -4,6 +4,7 @@ fun main(vararg args: String) {
 
     val start = args.filter { it.startsWith("-start") }.first()
     val command = args.filter { it.startsWith("-command") }.first().split("=")[1]
+    val commands = command.split(",")
     val obstacles: List<Pair<Int, Int>>? = args
         .filter { it.startsWith("-obstacles") }
         .getOrNull(0)
@@ -35,49 +36,51 @@ fun main(vararg args: String) {
     var newY = y
     var newD = d
 
-    val dirmsg = when (command) {
-        "f" -> "Rover moved forward"
-        "b" -> "Rover moved backward"
-        "l" -> "Rover turned left"
-        "r" -> "Rover turned right"
-        else -> "Invalid command '$command'"
+    for (cmd in commands) {
+        val dirmsg = when (cmd) {
+            "f" -> "Rover moved forward"
+            "b" -> "Rover moved backward"
+            "l" -> "Rover turned left"
+            "r" -> "Rover turned right"
+            else -> "Invalid command '$cmd'"
+        }
+
+        when (Pair(cmd, newD)) {
+            "f" to "n" -> newY++
+            "f" to "e" -> newX++
+            "f" to "s" -> newY--
+            "f" to "w" -> newX--
+            "b" to "n" -> newY--
+            "b" to "e" -> newX--
+            "b" to "s" -> newY++
+            "b" to "w" -> newX++
+            "l" to "n" -> newD = "w"
+            "l" to "e" -> newD = "n"
+            "l" to "s" -> newD = "e"
+            "l" to "w" -> newD = "s"
+            "r" to "n" -> newD = "e"
+            "r" to "e" -> newD = "s"
+            "r" to "s" -> newD = "w"
+            "r" to "w" -> newD = "n"
+        }
+
+        if (map != null && mapType == "w") {
+            if (newX == map.first) newX = 0
+            if (newX == -1) newX = map.first - 1
+            if (newY == map.second) newY = 0
+            if (newY == -1) newY = map.second - 1
+        }
+
+        val msg: String
+
+        if (obstacles != null && obstacles.contains(newX to newY)) {
+            msg = "Obstacle encountered at [$newX,$newY]. Current [$x,$y:$d]"
+        } else if (map != null && (newX < 0 || newX >= map.first || newY < 0 || newY >= map.second)) {
+            msg = "Boundary encountered at [$x,$y]. Current [$x,$y:$d]"
+        } else {
+            msg = "$dirmsg. Current [$newX,$newY:$newD]"
+        }
+
+        println(msg)
     }
-
-    when (Pair(command, d)) {
-        "f" to "n" -> newY++
-        "f" to "e" -> newX++
-        "f" to "s" -> newY--
-        "f" to "w" -> newX--
-        "b" to "n" -> newY--
-        "b" to "e" -> newX--
-        "b" to "s" -> newY++
-        "b" to "w" -> newX++
-        "l" to "n" -> newD = "w"
-        "l" to "e" -> newD = "n"
-        "l" to "s" -> newD = "e"
-        "l" to "w" -> newD = "s"
-        "r" to "n" -> newD = "e"
-        "r" to "e" -> newD = "s"
-        "r" to "s" -> newD = "w"
-        "r" to "w" -> newD = "n"
-    }
-
-    if (map != null && mapType == "w") {
-        if (newX == map.first) newX = 0
-        if (newX == -1) newX = map.first - 1
-        if (newY == map.second) newY = 0
-        if (newY == -1) newY = map.second - 1
-    }
-
-    val msg: String
-
-    if (obstacles != null && obstacles.contains(newX to newY)) {
-        msg = "Obstacle encountered at [$newX,$newY]. Current [$x,$y:$d]"
-    } else if (map != null && (newX < 0 || newX >= map.first || newY < 0 || newY >= map.second)) {
-        msg = "Boundary encountered at [$x,$y]. Current [$x,$y:$d]"
-    } else {
-        msg = "$dirmsg. Current [$newX,$newY:$newD]"
-    }
-
-    println(msg)
 }

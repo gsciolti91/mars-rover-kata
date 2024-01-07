d6ac14a256c8c69ce56601402ac0cd5f6a3d20fd
giacomo <giacomo.sciolti@gmail.com>
2024-01-07 11:47:11 +0100

    Refactoring - report andThen display

--- src/main/kotlin/com/gsciolti/kata/marsrover/domain/report/ReportingExecuteCommand.kt
+++ src/main/kotlin/com/gsciolti/kata/marsrover/domain/report/ReportingExecuteCommand.kt
@@ -6,22 +6,25 @@ import com.gsciolti.kata.marsrover.domain.command.execute.error.MarsRoverError
 import com.gsciolti.kata.marsrover.domain.model.Rover
 import com.gsciolti.kata.marsrover.domain.report.output.OutputChannel
 import com.gsciolti.kata.marsrover.functional.Either
+import com.gsciolti.kata.marsrover.functional.andThen
 
-class ReportingExecuteCommand<CMD, OUT> internal constructor(
-    private val delegateExecute: ExecuteCommand<CMD>,
-    private val reportCommandExecuted: ReportCommandExecuted<OUT>,
-    private val reportError: ReportError<OUT>,
-    private val outputChannel: OutputChannel<OUT>,
-) : ExecuteCommand<CMD> {
+class ReportingExecuteCommand<IN, OUT> internal constructor(
+    private val delegateExecute: ExecuteCommand<IN>,
+    reportCommandExecuted: ReportCommandExecuted<OUT>,
+    reportError: ReportError<OUT>,
+    outputChannel: OutputChannel<OUT>,
+) : ExecuteCommand<IN> {
 
-    override fun invoke(rover: Rover, command: CMD): Either<MarsRoverError, Pair<Command, Rover>> =
+    override fun invoke(rover: Rover, command: IN): Either<MarsRoverError, Pair<Command, Rover>> =
         delegateExecute(rover, command)
-            .tap { (command, updatedRover) ->
-                outputChannel.display(reportCommandExecuted(command, updatedRover))
-            }
-            .tapLeft { error ->
-                outputChannel.display(reportError(error))
-            }
+            .tap { (command, updatedRover) -> displayCommandExecuted(command, updatedRover) }
+            .tapLeft(displayError)
+
+    private val displayCommandExecuted: (Command, Rover) -> Unit =
+        reportCommandExecuted andThen outputChannel::display
+
+    private val displayError: (MarsRoverError) -> Unit =
+        reportError andThen outputChannel::display
 }
 
 fun <IN, OUT> ExecuteCommand<IN>.reportingWith(
--- src/main/kotlin/com/gsciolti/kata/marsrover/functional/Extensions.kt
+++ src/main/kotlin/com/gsciolti/kata/marsrover/functional/Extensions.kt
@@ -1,5 +1,11 @@
 package com.gsciolti.kata.marsrover.functional
 
+infix fun <A, B, C> ((A) -> B).andThen(g: (B) -> C): (A) -> C =
+    { a -> g(this(a)) }
+
+infix fun <A, B, C, D> ((A, B) -> C).andThen(g: (C) -> D): (A, B) -> D =
+    { a, b -> g(this(a, b)) }
+
 fun <T, L, R> Iterable<T>.update(initialState: R, updateState: (R, T) -> Either<L, R>): Either<L, R> =
     fold(initialState.right() as Either<L, R>) { currentState, nextInput ->
         currentState.flatMap { state ->
